734a21d84d69d56916b7d159003520e099b2810c
morantis <morantis2010@gmail.com>
2015-02-12 08:32:13 -0500

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -58,9 +58,9 @@
 #define CRYPTONOTE_PROTOCOL_HOP_RELAX_COUNT             3      //value of hop, after which we use only announce of new block
 
 //TODO This port will be used by the daemon to establish connections with p2p network
-#define P2P_DEFAULT_PORT                                
+#define P2P_DEFAULT_PORT                                4449                           
 //TODO This port will be used by the daemon to interact with simlewallet
-#define RPC_DEFAULT_PORT                                
+#define RPC_DEFAULT_PORT                                4448                          
 #define COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT           1000
 
 #define P2P_LOCAL_WHITE_PEERLIST_LIMIT                  1000
